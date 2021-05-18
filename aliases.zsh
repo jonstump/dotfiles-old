b3d2760b683ed81020dbb9cec9611460cc3f64ac
Jon Stump <jmstump@gmail.com>
2021-05-17 22:12:03 -0700

    change alias for neovim to v

--- aliases.zsh
+++ aliases.zsh
@@ -11,11 +11,9 @@ alias gp='git push'
 alias gclone='git clone'
 alias update='doas apt update && apt list --upgradeable && doas apt autoremove && doas apt upgrade && brew update && brew upgrade && flatpak update && doas snap refresh'
 alias icat="kitty +kitten icat"
-alias vim="nvim"
-alias vi="nvim"
+alias v="nvim"
 alias integrated="system76-power graphics integrated && sleep 20 && reboot"
 alias nvidia="system76-power graphics nvidia && sleep 20 && reboot"
-# alias sudo='doas'
 alias vpn='doas cyberghostvpn --connect --country-code US'
 alias vpnstop='doas cyberghostvpn --stop'
 alias vpnstat='cyberghostvpn --status'
@@ -23,3 +21,4 @@ alias license='mklicense'
 alias synology='doas mount.cifs //192.168.1.15/home /mnt/share -o credentials=/mount/credentials,uid=1000,gid=1000'
 alias spicetify=/home/jmstump/spicetify-cli/spicetify
 alias spicereload='spicetify backup apply enable-devtool'
+alias emailbox='ssh root@45.33.41.8'
